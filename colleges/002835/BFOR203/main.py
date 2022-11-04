c04295320525342bfa8c50cc402635ae60733cb2
Sl1mj1m14 <keiler104m@gmail.com>
2022-11-03 21:41:01 -0400

    Added the first functions to run question bank searches

--- colleges/002835/BFOR203/main.py
+++ colleges/002835/BFOR203/main.py
@@ -1,8 +1,10 @@
 #Any import statements for custom functions included in the answering function
 #should be listed at the top. Required imports are necessary.
 import re #REQUIRED - For Pattern Matching
+import json #REQUIRED - For searching through json of questions
 import importlib #REQUIRED - Necessary for trying to import files within this file
-ip_func = importlib.import_module("colleges.002835.BFOR203.ip") #OPTIONAL - Custom file for the math required on this test. Full path from colleges must be included
+dir = "colleges.002835.BFOR203" #REQUIRED - Specify the file location in order for custom files and the question bank to be loaded. Use periods to seperate directories
+ip_func = importlib.import_module(f"{dir}.ip") #OPTIONAL - Custom file for the math required on this test. Full path from colleges must be included
 
 #This is the function that should contain all the code necessary
 #to find an answer for a given question. It should list all
@@ -44,9 +46,20 @@ def return_answer (que):
         return ip_func.x_bin(que.split(" ")[len(que.split(" "))-1],0)
 
     #Before the final return statement, a question bank search should be completed if applicable
+    return search_question_bank(que)
     
     #Return undefined if the value is not found, this will ensure when submitting the user is warned these
-    #answers were missing
+    #answers were missing. Only use this if there is no question bank to search, and comment out the above 
+
+    #return None
+
+#This searches the local bank.json file for any question/answer pairs, and returns None if they are not found
+def search_question_bank (que):
+    dir2 = dir.replace(".","/")
+    with open(f"{dir2}/bank.json", "r") as bank:
+        parsed_bank = json.load(bank)
+        if que in parsed_bank:
+            return parsed_bank[que]
     return None
 
 
